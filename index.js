652b8f82351a8c8c5597575e7d0ab2e185d7c177
Ciobanu Dan <danu.ciobanu03@gmail.com>
2024-04-06 18:05:00 +0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+
+app.use(cors());
+
 app.get("/", (req, res) => {
-	res.send("hello world");
+	res.send({status: "alive"});
 }
 )
 
@@ -13,7 +17,7 @@ const PORT = 3001;
 
 const run = () => {
 	app.listen(PORT, () => {
-		console.log("Server is running on port", PORT)
+		console.log("Server is running on Ubuntu for port", PORT)
 	})
 }
 
