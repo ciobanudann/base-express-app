6506c7e248fef2882639597c21fd3c8295e72ebf
Ciobanu Dan <danu.ciobanu03@gmail.com>
2024-04-10 10:42:54 +0300

    format shit

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send({status: "alive"});
-}
-)
-
+  res.send({ status: "alive" });
+});
 
 const PORT = 3001;
 
-
 const run = () => {
-	app.listen(PORT, () => {
-		console.log("Server is running on Ubuntu for port", PORT)
-	})
-}
+  app.listen(PORT, () => {
+    console.log("Server is running on Ubuntu for port", PORT);
+  });
+};
 
-run()
+run();
